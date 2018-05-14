d46b1dcde7685d0c501505259203fcb50a91abdf
Naveen <naveen241193@gmail.com>
2018-05-14 09:07:15 +0530

    224344

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,8 +4,3 @@ export const FirstRunPage = 'LoginPage';
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
 export const MainPage = 'MenuPage';
-
-// The initial root pages for our tabs (remove if not using tabs)
-export const Tab1Root = 'ListMasterPage';
-export const Tab2Root = 'SearchPage';
-export const Tab3Root = 'SettingsPage';
